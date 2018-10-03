b10168c1e517eeff5b00786f2c700a3148e1be4c
raulcortazar <43799692+raulcortazar@users.noreply.github.com>
2018-10-02 21:02:30 -0400

    First Assignment

---
+++ P2T1_SalesPrediction_CortazarRaul.py
@@ -0,0 +1,11 @@
+# Finding the profit
+# 10/1/18
+# CTI-110 P2T1 - Sales Prediction
+# Raul Cortazar
+#
+
+totalSales = float(input("Enter the projected sales:"))
+
+profit = totalSales * .23
+
+print("The profit is: $", format(profit, ',.2f'))
